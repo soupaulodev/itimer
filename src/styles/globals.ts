cf85ec7178edacf110f15c26a7a2b5b26cc1a347
Paulo Marques <paulomarques.rs1@gmail.com>
2024-06-15 21:07:28 -0300

    fix: add antialised

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background: ${(props) => props.theme.colors["gray-900"]};
         color: ${(props) => props.theme.colors["gray-300"]};
+        -webbit-font-smoothing: antialiased;
     }
 
     body, input, button, textarea {
