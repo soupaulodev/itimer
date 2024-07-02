bcb34c38e6ce506a01635411e4a11a8f3725cc4c
Paulo Marques <paulomarques.rs1@gmail.com>
2024-07-02 00:33:20 -0300

    feat: Adionando timer ao titulo da pagina

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -88,6 +88,12 @@ export function Home() {
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
 
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [activeCycle, minutes, seconds]);
+
   const isSubmitDisabled = !watch("task") || !watch("minutesAmount");
 
   return (
